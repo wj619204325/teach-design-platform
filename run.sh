abab96df94f2a941893ab9a209f7b17f131b94c6
along <bnkalong@gmail.com>
2020-04-27 11:11:19 +0800

    修改run.shh

--- run.sh
+++ run.sh
@@ -1,10 +1,1 @@
-cd ./source
-npm install
-npm run build
-cd ./server
-npm install
-# 下面几步是把原来koa的server/public文件替换成新打包的dist中的文件
-# 并且把dist文件假删掉
-cd ..
-mv ./dist/* ./server/public/
-rm -rf ./dist
+npm install && npm run build && rm -rf ./server/public && mkdir ./server/public && mv ./dist/* ./server/public && rm -rf ./dist
