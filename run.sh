1d27f869ed846372a9a2adaeaaa2fcb42aae8078
along <bnkalong@gmail.com>
2020-04-27 12:09:42 +0800

    补充run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-npm install && npm run build && rm -rf ./server/public && mkdir ./server/public && mv ./dist/* ./server/public && rm -rf ./dist
+npm install && npm run build && rm -rf ./server/public && mkdir ./server/public && mv ./dist/* ./server/public && rm -rf ./dist && cd ./server && npm install
